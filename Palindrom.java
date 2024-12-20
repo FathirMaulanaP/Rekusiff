4479243d911428ed00593aff7c59ad3311afa871
FathirMaulanaP <fathirmaulanaputra83@gmail.com>
2024-12-20 09:27:04 +0700

    Add files via upload

---
+++ Palindrom.java
@@ -0,0 +1,22 @@
+public class Palindrom {
+    public static boolean palindrom(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return palindrom(str, start+1, end-1);
+    }
+public static void main(String[] args) {
+    String str ="sus";
+    Palindrom p = new Palindrom();
+    boolean result = p.palindrom(str, 0, str.length() -1);
+    if (result) {
+        System.out.println(str+ " :adalah palingdrom ");
+    }else{
+        System.out.println(str+ " :bukan palingdrom ");
+            }
+        }
+    }   
+
